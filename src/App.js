9106a6dd0dee7f3adac90623c9f6ddd7cf250300
Vishal Egbert <vishal_egbert@hotmail.com>
2020-09-26 01:51:48 +1000

    Bad attempt at auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React,  { useContext } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Home } from "./components/home";
 import { Register } from './components/register';
+import { Login } from './components/login';
+import { FirebaseContext } from "./utils/firebase";
 
 function App() {
+  const firebase = useContext(FirebaseContext);
+
+  function PrivateRoute ({component: Component, ...rest}) {
+    let authed = false;
+    
+    console.log(firebase.auth.currentUser);
+
+    return (
+      <Route
+        {...rest}
+        // Authed with firebase
+        render={(props) => firebase.auth.currentUser != null
+          ? <Component {...props} />
+          : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+      />
+    )
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/register">
-            <Register />
-          </Route>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <PrivateRoute exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/components/login.js
@@ -0,0 +1,67 @@
+import React,  { useContext } from 'react';
+import { FirebaseContext } from "../utils/firebase";
+import { Formik } from "formik";
+import { Form } from "react-bootstrap";
+import { FormikControl } from "formik-react-bootstrap";
+import '../styles/login.css';
+
+export const Login = () => {
+    const firebase = useContext(FirebaseContext);
+
+    function handleSubmit({email, password}) {
+        firebase.auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(error);
+          });
+
+        console.log(firebase.auth.currentUser)
+    }
+
+
+    return (
+        
+            
+                <Formik
+                initialValues={{
+                    age: 1,
+                }}
+                onSubmit={handleSubmit}>
+                {
+                    ({
+                    isSubmitting,
+                    handleSubmit,
+                    setFieldValue,
+                    }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <div class="text-center form-signin">
+                        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+                        <label for="inputEmail" class="sr-only">Email address</label>
+                        <FormikControl
+                            name="email"
+                            type="email"
+                            label="Email Address" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" />
+                        <FormikControl
+                            name="password"
+                            type="password"
+                            label="Password" class="sr-only"  placeholder="Password" />
+                        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+
+                        <br/>
+                        <p>or</p>
+                        
+
+                        <button class="btn btn-lg btn-primary btn-block"  onClick={ () => window.location.href='/register'}>Register</button>
+                        </div>
+                    </Form>
+                    )
+                }
+
+                </Formik> 
+
+
+    )
+ }
+
+ export default Login;
