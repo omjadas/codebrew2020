e1b7e2489f1fd6791b1b4b331820e2fda538f69c
omjadas <omja.das@gmail.com>
2020-09-26 09:59:33 +1000

    refactor: format code

--- src/components/home.js
+++ src/components/home.js
@@ -3,32 +3,28 @@ import { Nav } from 'react-bootstrap';
 import '../styles/home.css';
 
 export const Home = () => {
-  
-
   return (
-    
-  <>
-  <nav class="navbar sticky-top navbar-light bg-light">
-    <a class="navbar-brand" href="#">Project Awesome</a>
+    <>
+      <nav class="navbar sticky-top navbar-light bg-light">
+        <a class="navbar-brand" href="/">Project Awesome</a>
 
-    <div class="justify-content-end ">
-      <i className = "fas fa-search fa-lg nav-icons"></i>
-      <i class="far fa-user-circle fa-lg nav-icons"></i>
-    </div>
-  </nav>
+        <div class="justify-content-end ">
+          <i className = "fas fa-search fa-lg nav-icons"></i>
+          <i class="far fa-user-circle fa-lg nav-icons"></i>
+        </div>
+      </nav>
 
-  <Nav justify variant="tabs" defaultActiveKey="/home">
-    <Nav.Item>
-      <Nav.Link href="">Home</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link >Cal</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link >Appointments</Nav.Link>
-    </Nav.Item>
-  </Nav>
-</>
-
+      <Nav justify variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link >Cal</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link >Appointments</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
   );
 }
