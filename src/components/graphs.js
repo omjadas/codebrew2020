e03255d09189fdd3ec469257160ec3a02bfccb32
omjadas <omja.das@gmail.com>
2020-09-26 16:22:05 +1000

    chore: install react-vis

---
+++ src/components/graphs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  VerticalGridLines,
+  LineSeries
+} from "react-vis";
+
+const MSEC_DAILY = 86400000;
+
+export const Graphs = (props) => {
+
+};
