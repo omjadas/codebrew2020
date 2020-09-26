da382d1ab510025573496cfbd10ad0080d106a68
omjadas <omja.das@gmail.com>
2020-09-26 17:39:53 +1000

    refactor: add missing semicolons

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -10,18 +10,17 @@ export const Calendar = () => {
   const [days, setDays] = useState([]);
   const [minDay, setMinDay] = useState("2020-01-01");
   const [maxDay, setMaxDay] = useState("2020-01-01");
-
   const history = useHistory();
 
   useEffect(() => {
     firebase.user
       .then(user => {
-        return firebase.firestore.collection("entries").where("user", "==", user.email).get()
+        return firebase.firestore.collection("entries").where("user", "==", user.email).get();
       })
       .then(querySnapshot => {
         let resultDays = [];
-        let minDay = "9"
-        let maxDay = "0"
+        let minDay = "9";
+        let maxDay = "0";
 
         querySnapshot.forEach(doc => {
           let dateString = new Date(doc.data().time.seconds * 1000).toISOString().substr(0, 10);
@@ -29,22 +28,22 @@ export const Calendar = () => {
           resultDays.push({
             data: doc.data(),
             count: doc.data().attention + doc.data().communication + doc.data().difficultBehaviour + doc.data().overallMood + doc.data().sleepQuality + doc.data().socialInteraction,
-            date: dateString
+            date: dateString,
           })
 
           minDay = dateString.localeCompare(minDay) === -1 ? dateString : minDay;
           maxDay = dateString.localeCompare(maxDay) === 1 ? dateString : maxDay;
-        })
+        });
 
-        setDays(resultDays)
-        setMinDay(minDay)
-        setMaxDay(maxDay)
+        setDays(resultDays);
+        setMinDay(minDay);
+        setMaxDay(maxDay);
       })
   }, [firebase.firestore, firebase.user]);
 
   return (
     <>
-      <div className="calendar-wrapper">
+      <div className="ml-3 mr-0 mt-3 mb-3">
         <CalendarHeatmap
           startDate={minDay}
           endDate={maxDay}
@@ -56,9 +55,9 @@ export const Calendar = () => {
             if (value.count >= 6 && value.count <= 16) {
               return 'color-scale-red';
             } else if (value.count >= 17 && value.count <= 18) {
-              return 'color-scale-amber'
+              return 'color-scale-amber';
             } else if (value.count >= 19 && value.count <= 32) {
-              return 'color-scale-green'
+              return 'color-scale-green';
             }
           }}
           values={days}
